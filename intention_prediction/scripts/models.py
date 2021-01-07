95a9fc4d0c38b0f8a1683ee09fc493860bd17819
sgt390 <stefano.sgt39@gmail.com>
2021-01-07 14:36:28 +0100

    obslen30

--- intention_prediction/scripts/models.py
+++ intention_prediction/scripts/models.py
@@ -60,12 +60,12 @@ class CNNLSTM1(nn.Module):
         self.gradients = None
 
         # CNN Feature Extractor
-        self.model = models.densenet201(pretrained=True)
-        # outsize = self.model. fc.out_features
+        self.model = models.googlenet(pretrained=True)
+        outsize = self.model. fc.out_features
         self.model = nn.Sequential(self.model)  # *list(self.model.children())[0])
 
         # feature embedder
-        self.feature_embedder = nn.Linear(1000, embedding_dim)# (1536, embedding_dim)
+        self.feature_embedder = nn.Linear(outsize, embedding_dim)# (1536, embedding_dim)
 
         # LSTM
         self.lstm = nn.LSTM(embedding_dim, h_dim, 1, batch_first=False)
